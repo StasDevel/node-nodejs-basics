969e5658999f149c9ff1e41e92dffdd3b6938071
Stanislav Shkradov <studingDev@gmail.com>
2024-10-05 23:14:35 +0300

    feat: implement function of calculating hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import crypto from "crypto";
+import process from "process";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256");
+  hash.setEncoding("hex");
+
+  const readStream = fs.createReadStream(file);
+  readStream.on("data", (data) => {
+    hash.update(data);
+  });
+  readStream.on("end", () => {
+    process.stdout.write(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
